356297da6a3da0a22fde1d0603aced8152f1f539
EC2 Default User <ec2-user@ip-172-31-40-180.ec2.internal>
2021-07-09 15:48:42 +0000

    [Finish]2-6

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
-  
   #as: '名前' は「名前付きルート」といい、コード内でURLをわかりやすく書くために使わる。
   #ここでは、「'todolists#show'の設定を、todolistとして利用できる」の意味
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 
 end
